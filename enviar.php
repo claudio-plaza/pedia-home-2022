34d08cf7414b713a3a667241890854bca7d6bc3c
Claudio <95890307+claudio-plaza@users.noreply.github.com>
2022-08-23 21:52:52 -0300

    con las noticias echas

--- enviar.php
+++ enviar.php
@@ -13,7 +13,7 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $empresa . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'claudioplaza92@gmail.com';
+$para = 'pediahomesa@gmail.com';
 $asunto = 'Mensaje de mi sitio web';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
